3b2e385d424e8832cd108046bf0dc8feb9bd4525
retip94 <38405004+retip94@users.noreply.github.com>
2021-01-05 10:20:54 +0100

    day1 lettercase ignored in palindrom check

--- 1.py
+++ 1.py
@@ -1,4 +1,4 @@
 import webbrowser
 inp = input()
-print(inp[::-1], '\nis it palindrom? ', [c for c in inp if c.isalpha()] == [c for c in inp[::-1] if c.isalpha()])
+print(inp[::-1], '\nis it palindrom? ', [c for c in inp.lower() if c.isalpha()] == [c for c in inp[::-1].lower() if c.isalpha()])
 webbrowser.open(f'https://poocoo.pl/scrabble-slowa-z-liter/{inp}')
