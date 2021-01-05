a79fc3408278e066faf58efbcaa777df412303b2
retip94 <38405004+retip94@users.noreply.github.com>
2021-01-05 10:09:25 +0100

    day2 finished

---
+++ 2.py
@@ -0,0 +1,43 @@
+import requests, json
+from datetime import datetime
+import random
+from config import API_KEY
+import pytz
+from bs4 import BeautifulSoup
+
+# get current weather for given city
+city = 'Warszawa'
+r = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&lang=pl&appid={API_KEY}')
+if r.status_code == 200:
+    r = json.loads(r.content)
+    print(f'Prognoza dla miasta {city} o godzinie: {datetime.fromtimestamp(r["dt"])}')
+    print(f'{r["weather"][-1]["description"]}\n'
+          f'temperatura: {r["main"]["temp"]}°C\n'
+          f'ciśnienie: {r["main"]["pressure"]}hPa\n'
+          f'wilgotność: {r["main"]["humidity"]}%')
+else:
+    print(f'Błąd pobierania pogody: {r.status_code}')
+
+# get random quote
+r = requests.get('https://type.fit/api/quotes')
+if r.status_code == 200:
+    random_quote = random.choice(json.loads(r.content))
+    print(f'\n\"{random_quote["text"]}\"\n{random_quote["author"]}')
+else:
+    print(f'Błąd pobierania cytatu: {r.status_code}')
+
+# get current time in Washington, Sydney and Pekin
+print(f'\nWashington: {datetime.now(pytz.timezone("US/Pacific")).strftime("%H:%M:%S")}\n'
+      f'Sydney: {datetime.now(pytz.timezone("Australia/Sydney")).strftime("%H:%M:%S")}\n'
+      f'Shanghai: {datetime.now(pytz.timezone("Asia/Shanghai")).strftime("%H:%M:%S")}')
+
+# find whose name day is today
+r = requests.get('https://imienniczek.pl/widget/js')
+if r.status_code==200:
+    soup = BeautifulSoup(r.content, features='lxml').find_all('a')
+    names = [x.text for x in soup if x.text != "Imieniny"]
+    print(f'\nDzisiaj imieniny: {", ".join(names)}')
+else:
+    print(f'Błąd pobierania imienin: {r.status_code}')
+
+
